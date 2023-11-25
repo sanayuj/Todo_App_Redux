9c360a7f23b0813598222a5b989d749fbbf69e95
sanayuj <sanayuj2255@gmail.com>
2023-11-25 16:27:58 +0530

    ui design

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Todo() {
   return (
-    <div>Todo</div>
+    <div className='container my-4 d-flex justify-content-center ' ><form class="row g-3">
+   
+    <div class="col-auto">
+      <input type="text" class="form-control" id="todoInput" placeholder="Todo list"/>
+    </div>
+    <div class="col-auto">
+      <button type="submit" class="btn btn-primary mb-3">Add to Todo</button>
+    </div>
+  </form></div>
   )
 }
 
