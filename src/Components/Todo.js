be60bc3caab8e38823cc153d2a20f00e13a00059
sanayuj <sanayuj2255@gmail.com>
2023-11-25 17:15:08 +0530

    condition rendering

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -3,14 +3,11 @@ import { addTodo, removeTodo } from "../features/Todo/todoSlice";
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 import { useDispatch } from "react-redux";
 
-
-
-
 function Todo() {
-    const [input, setInput] = useState("");
-    const dispatch = useDispatch();
-    const todos=useSelector((state)=>state.todos)
-    console.log(todos,'sdsdsd')
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  console.log(todos, "sdsdsd");
 
   const handleTodoSubmit = (e) => {
     e.preventDefault();
@@ -18,37 +15,45 @@ function Todo() {
     setInput("");
   };
 
-
   return (
-   <>
-    <h2 className="text-center my-4">Redux Todo App</h2>
-    <div className="container my-4 d-flex justify-content-center ">
-    
-      <form onSubmit={handleTodoSubmit} className="row g-3">
-        <div className="col-auto">
-          <input
-            type="text"
-            className="form-control"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            id="todoInput"
-            placeholder="Todo list"
-          />
-        </div>
-        <div className="col-auto">
-          <button type="submit" className="btn btn-primary mb-3">
-            Add to Todo
-          </button>
-        </div>
-        
-        
-
-      </form>
+    <>
+      <h2 className="text-center my-4">Redux Todo App</h2>
+      <div className="container my-4 d-flex justify-content-center ">
+        <form onSubmit={handleTodoSubmit} className="row g-3">
+          <div className="col-auto">
+            <input
+              type="text"
+              className="form-control"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              id="todoInput"
+              placeholder="Todo list"
+            />
+          </div>
+          <div className="col-auto">
+            <button type="submit" className="btn btn-primary mb-3">
+              Add to Todo
+            </button>
+          </div>
+        </form>
       </div>
       <div className="text-center h-50">
-      <p>Todo List</p>
-      {todos.map((todo)=>(<div key={todo.id} className="col g-3  p-3 my-2" style={{backgroundColor:"indigo",color:"white"}}>{todo.text}</div>))}
-      
+        <p><b>Todo Lists</b></p>
+        {todos.length === 0 ? (
+          <div className="text-center mt-3">
+            <p>No activities to show.</p>
+          </div>
+        ) : (
+          todos.map((todo, index) => (
+            <div
+              key={todo.id}
+              className="col g-3 w-25 m-auto p-3 my-2"
+              style={{ backgroundColor: "indigo", color: "white" }}
+            >
+              {index + 1}: {todo.text}
+            </div>
+          ))
+        )}
       </div>
     </>
   );
