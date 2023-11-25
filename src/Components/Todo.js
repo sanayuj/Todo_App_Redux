fca85431b020b1a17a1228f72ae921d654ad03ab
sanayuj <sanayuj2255@gmail.com>
2023-11-25 16:38:38 +0530

    date store in redux

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { addTodo, removeTodo } from "../features/Todo/todoSlice";
+import { UseSelector } from "react-redux/es/hooks/useSelector";
+import { useDispatch } from "react-redux";
+
+
+
 
 function Todo() {
+    const [input, setInput] = useState("");
+    const dispatch = useDispatch();
+  const handleTodoSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
   return (
-    <div className='container my-4 d-flex justify-content-center ' ><form class="row g-3">
-   
-    <div class="col-auto">
-      <input type="text" class="form-control" id="todoInput" placeholder="Todo list"/>
+    <div className="container my-4 d-flex justify-content-center ">
+      <form onSubmit={handleTodoSubmit} class="row g-3">
+        <div class="col-auto">
+          <input
+            type="text"
+            class="form-control"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            id="todoInput"
+            placeholder="Todo list"
+          />
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3">
+            Add to Todo
+          </button>
+        </div>
+      </form>
     </div>
-    <div class="col-auto">
-      <button type="submit" class="btn btn-primary mb-3">Add to Todo</button>
-    </div>
-  </form></div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
