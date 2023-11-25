80e8f2bc6a1fc1f7f87eca1b46658f09d17c3f4c
sanayuj <sanayuj2255@gmail.com>
2023-11-25 17:02:18 +0530

    displayed submited data

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { addTodo, removeTodo } from "../features/Todo/todoSlice";
-import { UseSelector } from "react-redux/es/hooks/useSelector";
+import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 import { useDispatch } from "react-redux";
 
 
@@ -9,31 +9,48 @@ import { useDispatch } from "react-redux";
 function Todo() {
     const [input, setInput] = useState("");
     const dispatch = useDispatch();
+    const todos=useSelector((state)=>state.todos)
+    console.log(todos,'sdsdsd')
+
   const handleTodoSubmit = (e) => {
     e.preventDefault();
     dispatch(addTodo(input));
     setInput("");
   };
+
+
   return (
+   <>
+    <h2 className="text-center my-4">Redux Todo App</h2>
     <div className="container my-4 d-flex justify-content-center ">
-      <form onSubmit={handleTodoSubmit} class="row g-3">
-        <div class="col-auto">
+    
+      <form onSubmit={handleTodoSubmit} className="row g-3">
+        <div className="col-auto">
           <input
             type="text"
-            class="form-control"
+            className="form-control"
             value={input}
             onChange={(e) => setInput(e.target.value)}
             id="todoInput"
             placeholder="Todo list"
           />
         </div>
-        <div class="col-auto">
-          <button type="submit" class="btn btn-primary mb-3">
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary mb-3">
             Add to Todo
           </button>
         </div>
+        
+        
+
       </form>
-    </div>
+      </div>
+      <div className="text-center h-50">
+      <p>Todo List</p>
+      {todos.map((todo)=>(<div key={todo.id} className="col g-3  p-3 my-2" style={{backgroundColor:"indigo",color:"white"}}>{todo.text}</div>))}
+      
+      </div>
+    </>
   );
 }
 
